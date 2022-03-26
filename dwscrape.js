ab09ac12c5e1006a4c90b9eaef6ef05b6bb17559
ivxvm <ivxvm@protonmail.com>
2022-03-26 20:02:22 +0200

    Better downloads & edge cases handling

--- dwscrape.js
+++ dwscrape.js
@@ -4,19 +4,31 @@ import axios from 'axios';
 import { JSDOM } from 'jsdom';
 import * as R from 'ramda';
 import pThrottle from 'p-throttle';
+import util from 'util';
+import checksum from 'checksum';
+
+axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        console.log(`Request failed with status ${error.response.status} - ${error.response.statusText}`);
+        return Promise.reject(error);
+    }
+);
 
 const BASE_URL = 'https://www.doomworld.com/forum/4-wads-mods';
 const DB_FILE_PATH = './db.json';
 const ATTACHMENTS_DIR_PATH = './attachments';
+const MAX_PAGES_TO_PROCESS = 5;
 
 const SELECTORS = {
     pagination: '.ipsPagination_pageJump',
     forumThreadLink: '.ipsDataItem_title a[data-ipshover-target]',
     threadPost: '.cPost',
-    threadPost_author: '.ipsComment_author .cAuthorPane_author a',
+    threadPost_author: '.ipsComment_author .cAuthorPane_author',
     threadPost_text: '.cPost_contentWrap .ipsType_richText',
     threadPost_text_iframe: '.cPost_contentWrap .ipsType_richText iframe',
-    threadPost_attachment: 'a.ipsAttachLink',
+    threadPost_text_image: '.cPost_contentWrap .ipsType_richText img',
+    threadPost_attachment: 'a.ipsAttachLink:not(.ipsAttachLink_image)',
 };
 
 if (!fs.existsSync(DB_FILE_PATH)) {
@@ -51,9 +63,17 @@ const downloadFile = (url, path) =>
             .catch(reject);
     });
 
+console.log('Reading database');
+const db = JSON.parse(fs.readFileSync(DB_FILE_PATH));
+
+const saveDb = () => {
+    console.log('Saving database to file');
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify(db, null, 4));
+};
+
+setInterval(saveDb, 20_000);
+
 (async () => {
-    console.log('Reading database');
-    const db = JSON.parse(fs.readFileSync(DB_FILE_PATH));
     let lastPage = 1;
     for (let page = 1; page <= lastPage; page++) {
         console.log(`Processing page ${page} of forum`);
@@ -65,14 +85,23 @@ const downloadFile = (url, path) =>
         if (typeof forumPagesCount !== 'number') {
             throw new Error(`pagesCount is not a number: ${forumPagesCount}`);
         }
-        lastPage = Math.max(lastPage, forumPagesCount);
+        lastPage = Math.min(MAX_PAGES_TO_PROCESS, Math.max(lastPage, forumPagesCount));
         for (const link of forumDocument.querySelectorAll(SELECTORS.forumThreadLink)) {
-            console.log(`Processing thread ${link.href} - ${link.textContent.trim()}`);
+            const threadTitle = link.textContent.trim();
+            console.log(`Processing thread ${link.href} - ${threadTitle}`);
             const threadId = link.href.match(/topic\/(\d+)-/)[1];
             assert.ok(threadId);
             console.log(`Thread id = ${threadId}`);
-            let lastThreadPage = 1;
-            for (let threadPage = 1; threadPage <= lastThreadPage; threadPage++) {
+            const thread = db[threadId] || {
+                title: threadTitle,
+                pagesProcessed: 1,
+                posts: {},
+            };
+            if (thread.pagesProcessed > 1) {
+                console.log(`Found thread records in database, continue processing from page ${thread.pagesProcessed}`);
+            }
+            let lastThreadPage = thread.pagesProcessed;
+            for (let threadPage = thread.pagesProcessed; threadPage <= lastThreadPage; threadPage++) {
                 console.log(`Processing page ${threadPage} of thread ${threadId}`);
                 const threadResponse = await throttle(axios.get)(link.href + '?page=' + threadPage);
                 const threadDom = new JSDOM(threadResponse.data);
@@ -85,29 +114,38 @@ const downloadFile = (url, path) =>
                     console.log('Thread has up-to-date data in database already, skipping');
                     break;
                 }
-                const thread = db[threadId] || {
-                    pagesProcessed: 0,
-                    posts: {},
-                };
                 assert.strictEqual(typeof threadPagesCount, 'number');
                 lastThreadPage = Math.max(lastThreadPage, threadPagesCount);
                 for (const postNode of threadDocument.querySelectorAll(SELECTORS.threadPost)) {
                     const postId = postNode.id.trim().split('_').at(-1);
                     assert.ok(postId);
-                    const postAuthor = postNode.querySelector(SELECTORS.threadPost_author).textContent;
+                    const postAuthor = postNode.querySelector(SELECTORS.threadPost_author).textContent.trim();
                     assert.ok(postAuthor);
-                    console.log(`Replacing iframes in post ${postId}`);
-                    for (const iframe of postNode.querySelectorAll(SELECTORS.threadPost_text_iframe)) {
-                        const url = iframe.src;
-                        const link = threadDocument.createElement('a');
-                        link.href = url;
-                        link.textContent = url;
-                        iframe.replaceWith(link);
+                    if (postNode.querySelector(SELECTORS.threadPost_text_iframe)) {
+                        console.log(`Replacing iframes in post ${postId}`);
+                        for (const iframe of postNode.querySelectorAll(SELECTORS.threadPost_text_iframe)) {
+                            const url = iframe.src;
+                            const link = threadDocument.createElement('a');
+                            link.href = url;
+                            link.textContent = url;
+                            link.dataset.replacement = true;
+                            iframe.replaceWith(link);
+                        }
+                    }
+                    if (postNode.querySelector(SELECTORS.threadPost_text_image)) {
+                        console.log(`Replacing images in post ${postId}`);
+                        for (const image of postNode.querySelectorAll(SELECTORS.threadPost_text_image)) {
+                            const url = image.src;
+                            const link = threadDocument.createElement('a');
+                            link.href = url;
+                            link.textContent = url;
+                            link.dataset.replacement = true;
+                            image.replaceWith(link);
+                        }
                     }
                     const postText = postNode.querySelector(SELECTORS.threadPost_text).textContent.trim();
                     assert.ok(postText);
                     const post = {
-                        id: postId,
                         author: postAuthor,
                         text: postText,
                         attachments: [],
@@ -118,23 +156,51 @@ const downloadFile = (url, path) =>
                             .replace(/\r?\n/g, ' ')}..."`
                     );
                     for (const attachment of postNode.querySelectorAll(SELECTORS.threadPost_attachment)) {
+                        if (attachment.textContent.trim() === '' || attachment.querySelector('[data-replacement]')) {
+                            console.log(`Bad attachment ${attachment.href}, skipping`);
+                            continue;
+                        }
                         let url = attachment.href;
                         if (url.startsWith('//')) {
                             url = 'https:' + url;
                         }
                         const title = attachment.textContent.trim();
                         const dlPath = `${ATTACHMENTS_DIR_PATH}/${threadId}_${postId}_${title}`;
-                        console.log(`Downloading attachment ${url} to ${dlPath}`);
-                        await downloadFile(url, dlPath);
-                        post.attachments.push({ url, title });
+                        try {
+                            console.log(`Downloading attachment ${url}`);
+                            await downloadFile(url, dlPath + '_temp');
+                            let renamedFilePath = null;
+                            let oldFileChecksum = null;
+                            if (fs.existsSync(dlPath)) {
+                                console.log('Corresponding attachment file already exists, renaming old file');
+                                renamedFilePath = `${ATTACHMENTS_DIR_PATH}/${threadId}_${postId}_${Date.now()}_${title}`;
+                                oldFileChecksum = await util.promisify(checksum.file)(dlPath);
+                                fs.renameSync(dlPath, renamedFilePath);
+                                post.attachments = post.attachments.map((att) =>
+                                    att.path === dlPath ? { ...att, path: renamedFilePath } : att
+                                );
+                            }
+                            if (renamedFilePath) {
+                                const newFileChecksum = await util.promisify(checksum.file)(dlPath);
+                                if (newFileChecksum === oldFileChecksum) {
+                                    console.log('Old and new file checksum are the same, removing old file');
+                                    fs.rmSync(renamedFilePath);
+                                }
+                            }
+                            console.log(`Renaming temp file to ${dlPath}`);
+                            fs.renameSync(dlPath + '_temp', dlPath);
+                            post.attachments.push({ url, title, path: dlPath });
+                        } catch (error) {
+                            console.log('Download failed, skipping');
+                        }
                     }
                     thread.posts[postId] = post;
                 }
-                thread.pagesProcessed = threadPagesCount;
+                thread.pagesProcessed = threadPage;
                 db[threadId] = thread;
             }
-            fs.writeFileSync(DB_FILE_PATH, JSON.stringify(db));
+            console.log(`Finished processing thread ${threadId}`);
         }
-        process.exit();
     }
+    saveDb();
 })();
